192d2fe1d20d5e2d6c2f98528579fe0f751246a3
dearyfareq <dearyfareq15@gmail.com>
2022-08-18 00:02:08 +0300

    added the components into the main app file

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Header from './components/header/header'
 import Navi from './components/navi/navi'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Service from './components/service/service'
+import Portfolio from './components/portfolio/portfolio'
+import Testi from './components/testi/testi'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 const App = () => {
     return(
         <>
         <Header />
         <Navi />
+        <About />
+        <Experience />
+        <Service />
+        <Portfolio />
+        <Testi />
+        <Contact />
+        <Footer />
         </>
     )
 }
