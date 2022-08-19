49c8dfae45bc38a622d0061b2fc194a3715031eb
dearyfareq <dearyfareq15@gmail.com>
2022-08-19 14:51:56 +0300

    imported linkedin icon

--- capstone/src/components/header/headersocials.jsx
+++ capstone/src/components/header/headersocials.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { SiLinkedin } from "react-icons/si";
+import React from "react"
+import {BsLinkedin} from "react-icons/bs"
 
 const Headersocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
-        <SiLinkedin />
-      </a>
+      <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
       <a href="https://github.com" target="_blank"></a>
       <a href="https://dribble.com" target="_blank"></a>
     </div>
