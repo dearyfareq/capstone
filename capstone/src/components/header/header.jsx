155948e9c59df385c38371847f4b788343cdcff6
dearyfareq <dearyfareq15@gmail.com>
2022-08-19 15:28:11 +0300

    updated the header img

--- capstone/src/components/header/header.jsx
+++ capstone/src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "./header.css"
 import CTA from "../header/CTA"
-import ME from "../../assests/ME.jpg"
+import ME from "../../assests/ME.png"
 import Headersocials from "./headersocials"
 const Header = () => {
   return (
@@ -12,11 +12,10 @@ const Header = () => {
          <h5 className="text-light">Mechatronical Engineer</h5>
          <CTA/>
          <Headersocials/>
-
          <div className="me">
           <img src={ME} alt="me" />
          </div>
-         <a href="#Contact" className="scroll_down">Scroll Down</a>
+         <a href="#Contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   );
