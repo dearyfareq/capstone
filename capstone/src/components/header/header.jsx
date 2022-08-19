69527536f2ae1666410481c6657274a4b6b2c6fb
dearyfareq <dearyfareq15@gmail.com>
2022-08-19 14:19:31 +0300

    half done the header
    added CTA and buttons

--- capstone/src/components/header/header.jsx
+++ capstone/src/components/header/header.jsx
@@ -1,9 +1,20 @@
-import React from "react";
-import "./header.css";
+import React from "react"
+import "./header.css"
+import CTA from "../header/CTA"
+import ME from ""
 const Header = () => {
   return (
     <header>
-      <div className="container header__container"></div>
+      <div className="container header__container">
+         <h5>Hello! I am</h5>
+         <h1>Diyari Fariq</h1>
+         <h5 className="text-light">Mechatronical Engineer</h5>
+         <CTA/>
+
+         <div className="me">
+          <img src={ME} alt="me" />
+         </div>
+      </div>
     </header>
   );
 };
