0c9bc6ff53f0b467f83b9ec7f0de8b71d20ebe40
dearyfareq <dearyfareq15@gmail.com>
2022-08-19 14:57:20 +0300

    added behance and github icons

--- capstone/src/components/header/headersocials.jsx
+++ capstone/src/components/header/headersocials.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import {BsLinkedin} from "react-icons/bs"
+import {FaGithubSquare} from "react-icons/fa"
+import {BsBehance} from "react-icons/bs"
 
 const Headersocials = () => {
   return (
     <div className="header__socials">
       <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com" target="_blank"></a>
-      <a href="https://dribble.com" target="_blank"></a>
+      <a href="https://github.com" target="_blank"><FaGithubSquare/></a>
+      <a href="https://www.behance.net/" target="_blank"><BsBehance/></a>
     </div>
   );
 };
