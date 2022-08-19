2bb1b6a026f5a4f214f307dc50bcbaed7bafeb47
dearyfareq <dearyfareq15@gmail.com>
2022-08-19 15:10:01 +0300

    fixed warnings

--- capstone/src/components/header/headersocials.jsx
+++ capstone/src/components/header/headersocials.jsx
@@ -3,12 +3,13 @@ import {BsLinkedin} from "react-icons/bs"
 import {FaGithubSquare} from "react-icons/fa"
 import {BsBehance} from "react-icons/bs"
 
+
 const Headersocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com" target="_blank"><FaGithubSquare/></a>
-      <a href="https://www.behance.net/" target="_blank"><BsBehance/></a>
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+      <a href="https://github.com" target="_blank" rel="noreferrer"><FaGithubSquare/></a>
+      <a href="https://www.behance.net/" target="_blank" rel="noreferrer"><BsBehance/></a>
     </div>
   );
 };
