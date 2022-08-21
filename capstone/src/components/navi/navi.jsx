ed97c93e02b68e531f8e9beffd14a6650e7e0e6b
dearyfareq <dearyfareq15@gmail.com>
2022-08-21 15:58:04 +0300

    finalized the nav-bar with an imported function(use state))

--- capstone/src/components/navi/navi.jsx
+++ capstone/src/components/navi/navi.jsx
@@ -7,13 +7,14 @@ import {RiServiceFill} from 'react-icons/ri'
 import {MdMessage} from 'react-icons/md'
 import {useState} from 'react'
 const Navi = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#header" className='active'><FaHome/></a>
-      <a href="#about"><FaUserTie/></a>
-      <a href="#experience"><GiNotebook/></a>
-      <a href="#service"><RiServiceFill/></a>
-      <a href="#contact"><MdMessage/></a>
+      <a href="#header" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserTie/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiNotebook/></a>
+      <a href="#service" onClick={() => setActiveNav('#service')} className={activeNav === '#service' ? 'active' : ''}><RiServiceFill/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdMessage/></a>
     </nav>
   )
 }
