a887b99245226757d6223faa08f10c50e3efef63
dearyfareq <dearyfareq15@gmail.com>
2022-08-21 15:28:52 +0300

    added nav-bar icons and imported them

--- capstone/src/components/navi/navi.jsx
+++ capstone/src/components/navi/navi.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './navi.css'
+import {FaHome} from 'react-icons/fa'
+import {FaUserTie} from 'react-icons/fa'
+import {GiNotebook} from 'react-icons/gi'
+import {RiServiceFill} from 'react-icons/ri'
+import {MdMessage} from 'react-icons/md'
 const Navi = () => {
   return (
-    <div>navi</div>
+    <nav>
+      <a href="#header"><FaHome/></a>
+      <a href="#about"><FaUserTie/></a>
+      <a href="#experience"><GiNotebook/></a>
+      <a href="#service"><RiServiceFill/></a>
+      <a href="#contact"><MdMessage/></a>
+    </nav>
   )
 }
 
