66a4340f8e2c244a1f50965320ba06315a7cbe20
dearyfareq <dearyfareq15@gmail.com>
2022-08-21 15:46:42 +0300

    added nav-bar CSS

--- capstone/src/components/navi/navi.jsx
+++ capstone/src/components/navi/navi.jsx
@@ -5,10 +5,11 @@ import {FaUserTie} from 'react-icons/fa'
 import {GiNotebook} from 'react-icons/gi'
 import {RiServiceFill} from 'react-icons/ri'
 import {MdMessage} from 'react-icons/md'
+import {useState} from 'react'
 const Navi = () => {
   return (
     <nav>
-      <a href="#header"><FaHome/></a>
+      <a href="#header" className='active'><FaHome/></a>
       <a href="#about"><FaUserTie/></a>
       <a href="#experience"><GiNotebook/></a>
       <a href="#service"><RiServiceFill/></a>
