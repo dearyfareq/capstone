e559bf95c0ba36f5183c34ae235e3e91544ffabf
dearyfareq <dearyfareq15@gmail.com>
2022-08-23 23:30:29 +0300

    added styling and some CSS to icons

--- capstone/src/components/experience/experience.jsx
+++ capstone/src/components/experience/experience.jsx
@@ -12,37 +12,37 @@ const Experience = () => {
           <h3>frontend development</h3>
           <div className='experience__content'>
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>HTML</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>CSS</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>JAVA</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>REACT</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>PYTHON</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>GITHUB</h4>
               <small className='text-light'>Experienced</small>
             </article>
@@ -53,37 +53,37 @@ const Experience = () => {
         <h3>Backend development</h3>
           <div className='experience__content'>
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>WELDING</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>WOODWORKING</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>ARDUINO</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>ROBOTICS</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>DESGIN</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className='experince__details'>
-              <BsCheck2Square/>
+              <BsCheck2Square className='experience__details-icon'/>
               <h4>ILLUSTRATION</h4>
               <small className='text-light'>Experienced</small>
             </article>
