05c94dafa25effa8edf53824e277307282620814
dearyfareq <dearyfareq15@gmail.com>
2022-08-25 22:37:29 +0300

    added CSS to testimonials and fixed the image ratio problem

--- capstone/src/components/testi/testi.jsx
+++ capstone/src/components/testi/testi.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import './testi'
-import IMG1 from '../../assests/avatar 1.png'
-import IMG2 from '../../assests/avatar 2.png'
+import React from "react";
+import "./testi.css";
+import IMG1 from "../../assests/avatar 1.png";
+import IMG2 from "../../assests/avatar 2.png";
 const Testi = () => {
   return (
-    <section id='testi'>
+    <section id="testi">
       <h5>Review from others</h5>
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className='testimonials'>
-          <div className='avatar'>
-            <img src={IMG1} alt="avatar" />
-            <h5 className='avatar__name'>Dr.hawre</h5>
-            <small className='review'>
-              lorem 
-            </small>
+        <article className="testimonials">
+          <div className="avatar">
+            <img src={IMG1} alt="avata" />
           </div>
+          <h5 className="avatar__name">Dr.hawre</h5>
+          <small className="review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, iusto
+            praesentium. Repellat, numquam quidem esse animi nihil quas neque
+            quam repudiandae hic possimus, ipsam ipsum debitis consequatur
+            veritatis? Voluptates, illum?
+          </small>
         </article>
 
-        <article className='testimonials'>
-          <div className='avatar'>
+        <article className="testimonials">
+          <div className="avatar">
             <img src={IMG2} alt="avatar" />
-            <h5 className='avatar__name'>Dr.Dilshad</h5>
-            <small className='review'>
-              lorem 
-            </small>
           </div>
+          <h5 className="avatar__name">Dr.Dilshad</h5>
+          <small className="review">    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, iusto
+            praesentium. Repellat, numquam quidem esse animi nihil quas neque
+            quam repudiandae hic possimus, ipsam ipsum debitis consequatur
+            veritatis? Voluptates, illum?
+            </small>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testi
+export default Testi;
