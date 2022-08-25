4ae3720b1967e826609674dcd6c03cf822f24a37
dearyfareq <dearyfareq15@gmail.com>
2022-08-25 22:07:09 +0300

    tried to add a map, didn't work XD

--- capstone/src/components/portfolio/portfolio.jsx
+++ capstone/src/components/portfolio/portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './portfolio.css'
+import '../portfolio/portfolio.css'
 import IMG1 from '../../assests/portfolio1.jpg'
 import IMG2 from '../../assests/portfolio2.jpg'
 import IMG3 from '../../assests/portfolio3.jpg'
@@ -7,44 +7,11 @@ import IMG4 from '../../assests/portfolio4.jpg'
 import IMG5 from '../../assests/portfolio5.png'
 import IMG6 from '../../assests/portfolio6.jpg'
 
-const data =[
-  {id: 1,
-  image: IMG1,
-  title:'title1',
-  github:'link'
-  },
-  {id: 2,
-    image: IMG1,
-    title:'title1',
-    github:'link',
-  },
-  {id: 3,
-      image: IMG1,
-      title:'title1',
-      github:'link'
-  },
-  {id: 4,
-    image: IMG1,
-    title:'title1',
-    github:'link'
-  },
-  {id: 5,
-    image: IMG1,
-    title:'title1',
-    github:'link'
-  },
-  {id: 6,
-    image: IMG1,
-    title:'title1',
-    github:'link'
-  }
-]
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My privous work</h5>
       <h2>Portfolio</h2>
-
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
           <div className='portfolio__item-img'>
@@ -53,7 +20,6 @@ const Portfolio = () => {
           <h3>this is a portfolio item title</h3>
           <div className='portfolio__item-cta'>
           <a href="https://github.com" className='btn' rel="noreferrer" target='_blank'>Github</a>
-          <a href="https://www.behance.net/" className='btn btn-prime' rel="noreferrer" target='_blank'>Behance</a>
           </div>
         </article>
 
@@ -64,7 +30,6 @@ const Portfolio = () => {
           <h3>this is a portfolio item title</h3>
           <div className='portfolio__item-cta'>
           <a href="https://github.com" className='btn' rel="noreferrer" target='_blank'>Github</a>
-          <a href="https://www.behance.net/" className='btn btn-prime' rel="noreferrer" target='_blank'>Behance</a>
           </div>
         </article>
 
@@ -75,7 +40,6 @@ const Portfolio = () => {
           <h3>this is a portfolio item title</h3>
           <div className='portfolio__item-cta'>
           <a href="https://github.com" className='btn' rel="noreferrer" target='_blank'>Github</a>
-          <a href="https://www.behance.net/" className='btn btn-prime' rel="noreferrer" target='_blank'>Behance</a>
           </div>
         </article>
 
@@ -86,7 +50,6 @@ const Portfolio = () => {
           <h3>this is a portfolio item title</h3>
           <div className='portfolio__item-cta'>
           <a href="https://github.com" className='btn' rel="noreferrer" target='_blank'>Github</a>
-          <a href="https://www.behance.net/" className='btn btn-prime' rel="noreferrer" target='_blank'>Behance</a>
           </div>
         </article>
 
@@ -97,7 +60,6 @@ const Portfolio = () => {
           <h3>this is a portfolio item title</h3>
           <div className='portfolio__item-cta'>
           <a href="https://github.com" className='btn' rel="noreferrer" target='_blank'>Github</a>
-          <a href="https://www.behance.net/" className='btn btn-prime' rel="noreferrer" target='_blank'>Behance</a>
           </div>
         </article>
 
@@ -108,7 +70,6 @@ const Portfolio = () => {
           <h3>this is a portfolio item title</h3>
           <div className='portfolio__item-cta'>
           <a href="https://github.com" className='btn' rel="noreferrer" target='_blank'>Github</a>
-          <a href="https://www.behance.net/" className='btn btn-prime' rel="noreferrer" target='_blank'>Behance</a>
           </div>
         </article>
       </div>
