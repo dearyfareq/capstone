764cbcb989844e0dc3da01c574e222ca29c41049
dearyfareq <dearyfareq15@gmail.com>
2022-08-26 18:04:14 +0300

    added footer jsx and css

--- capstone/src/components/footer/footer.jsx
+++ capstone/src/components/footer/footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
       <a href="#" className='footer__logo'>Diyari</a>
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="#about">Home</a></li>
-        <li><a href="#experience">Home</a></li>
-        <li><a href="#service">Home</a></li>
-        <li><a href="#portfolio">Home</a></li>
-        <li><a href="#testimonials">Home</a></li>
-        <li><a href="#contact">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experince</a></li>
+        <li><a href="#service">Service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className='footer__socials'>
