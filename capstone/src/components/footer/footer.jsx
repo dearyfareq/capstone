92b9178d2dffd4b784737a574b510e44e17f0285
dearyfareq <dearyfareq15@gmail.com>
2022-08-26 20:47:08 +0300

    hotfixes

--- capstone/src/components/footer/footer.jsx
+++ capstone/src/components/footer/footer.jsx
@@ -6,21 +6,21 @@ import {BsInstagram} from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Diyari</a>
+      <a href="#home" className='footer__logo' rel="noreferrer">Diyari</a>
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experince</a></li>
-        <li><a href="#service">Service</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#home" rel="noreferrer">Home</a></li>
+        <li><a href="#about" rel="noreferrer">About</a></li>
+        <li><a href="#experience" rel="noreferrer">Experince</a></li>
+        <li><a href="#service" rel="noreferrer">Service</a></li>
+        <li><a href="#portfolio" rel="noreferrer">Portfolio</a></li>
+        <li><a href="#testimonials" rel="noreferrer">testimonials</a></li>
+        <li><a href="#contact" rel="noreferrer">Contact</a></li>
       </ul>
 
       <div className='footer__socials'>
-        <a href="https://www.facebook.com/deary.fareq/"><AiOutlineFacebook/></a>
-        <a href="https://github.com/dearyfareq"><AiFillGithub/></a>
-        <a href="facebook"><BsInstagram/></a>
+        <a href="https://www.facebook.com/deary.fareq/" rel="noreferrer"><AiOutlineFacebook/></a>
+        <a href="https://github.com/dearyfareq" rel="noreferrer"><AiFillGithub/></a>
+        <a href="https://www.instagram.com/dearyfareq/" rel="noreferrer"> <BsInstagram/></a>
       </div>
 
       <div className='footer__copyright'>
